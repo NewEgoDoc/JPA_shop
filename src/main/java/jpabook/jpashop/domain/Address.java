3f9dcf111f2efb4569b1d4f6b9df0c53a6de7ea4
Juhan <juhan211@naver.com>
2022-01-28 23:49:55 +0900

    엔티티 클래스 개발2

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -6,9 +6,13 @@ import lombok.Setter;
 import javax.persistence.Embeddable;
 
 @Embeddable
-@Getter @Setter
+@Getter
 public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address() {
+
+    }
 }
--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,10 +1,12 @@
 package jpabook.jpashop.domain.item;
 
+import jpabook.jpashop.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.context.annotation.EnableMBeanExport;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
@@ -20,5 +22,9 @@ public abstract class Item {
     private int price;
     private int stockQuantity;
 
-
+    @ManyToMany
+    @JoinTable(name = "category_item",
+               joinColumns = @JoinColumn(name = "category_id"),
+               inverseJoinColumns = @JoinColumn(name = "item_id"))
+    private List<Category> categories = new ArrayList<>();
 }
