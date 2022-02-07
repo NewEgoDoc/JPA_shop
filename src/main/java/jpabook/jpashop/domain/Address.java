7b51ae058f9fe93357c849656599e09eb1635a2b
Juhan <juhan211@naver.com>
2022-02-07 22:36:34 +0900

    주문 기능 테스트

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,18 +1,15 @@
 package jpabook.jpashop.domain;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Address {
     private String city;
     private String street;
     private String zipcode;
-
-    protected Address() {
-
-    }
 }
